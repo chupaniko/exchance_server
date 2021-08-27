9d9d1592f38815ec18528538d5db2ebdaf60e776
nicol <nikolaicxxi@gmail.com>
2021-08-27 09:25:34 +0400

    add sending emails from gmail

--- src/main/java/com/example/exchance_server/email/EmailService.java
+++ src/main/java/com/example/exchance_server/email/EmailService.java
@@ -3,13 +3,18 @@ package com.example.exchance_server.email;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import javax.mail.Address;
 import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 @Service
 @AllArgsConstructor
@@ -24,14 +29,29 @@ public class EmailService implements EmailSender {
     @Async
     public void send(String to, String email) {
         try{
+            Properties emailProperties = System.getProperties();
+            emailProperties.put("mail.smtp.port", "587");
+            emailProperties.put("mail.smtp.auth", "true");
+            emailProperties.put("mail.smtp.starttls.enable", "true");
+            Session mailSession = Session.getDefaultInstance(System.getProperties(), null);
+
+            String botEmail = "aethleet@gmail.com";
+            String botPassword = "n0n4cann0n";
+
+            String emailHost = "smtp.gmail.com";
+            Transport transport = mailSession.getTransport("smtp");
+            transport.connect(emailHost, botEmail, botPassword);
+
             MimeMessage mimeMessage = mailSender.createMimeMessage();
             MimeMessageHelper helper =
                     new MimeMessageHelper(mimeMessage, "utf-8");
             helper.setText(email, true);
             helper.setTo(to);
             helper.setSubject("Confirm your email");
-            helper.setFrom("exchance@ssau.ru");
-            mailSender.send(mimeMessage);
+            //helper.setFrom("exchance@ssau.ru");
+            transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
+            transport.close();
+            //mailSender.send(mimeMessage);
         } catch (MessagingException e) {
             LOGGER.error("failed to send email", e);
             throw new IllegalStateException("failed to send email");
--- src/main/java/com/example/exchance_server/registration/RegistrationService.java
+++ src/main/java/com/example/exchance_server/registration/RegistrationService.java
@@ -7,6 +7,7 @@ import com.example.exchance_server.email.EmailSender;
 import com.example.exchance_server.registration.token.ConfirmationToken;
 import com.example.exchance_server.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
